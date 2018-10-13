1bef3e6186362110299160f7c40e5d89884b871e
shellyHBG <k820720k@gmail.com>
2018-10-13 22:50:59 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ],
+  externals: {
+    "pixi.js": "PIXI"
+  }
+};
