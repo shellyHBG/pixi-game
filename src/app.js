eef7c4424b9a681c0859e71688d5848bb272c71a
kayac <egg734631@gmail.com>
2018-10-15 00:58:01 +0800

    Add loader

--- src/app.js
+++ src/app.js
@@ -6,10 +6,13 @@
  */
 
 //  ============== Dependencies =========================
-import './application.css';
+import './app.css';
 import createApp from './facade/application';
-
+import loader from './facade/load';
+import imgToriel from './assets/toriel.png';
 //  ============== Execution =========================
 document.body.appendChild(createApp().view);
+
+loader.add('toriel', imgToriel);
 
 
--- src/facade/load.js
+++ src/facade/load.js
@@ -1,2 +1,15 @@
+// @flow
+
+/*
+ *  Description: Interface for Getting PIXI Application
+ *  Author: Kayac
+ */
+
+//  ============== Dependencies =========================
 import {loader} from 'pixi.js';
+
+//  ============== Function =========================
+loader.load(console.log);
+
+export default loader;
 
