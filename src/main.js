c7542d780478e839c82f60f6904de90613c335ec
shellyHBG <k820720k@gmail.com>
2018-10-18 18:06:29 +0800

    eventmanager example

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-import * as PIXI from 'pixi.js';
+// import * as PIXI from 'pixi.js';
+// console.log(PIXI);
 
-console.log(PIXI);
+// event manager test
+import { EventManager, TestEvent } from './foundation/eventManager.js';
+
+let registerEventIndex = EventManager.RegisterEventListner(TestEvent, this, ()=>{ console.log("test event triger!!!"); });
+EventManager.Update();
+
+let testEvent = new TestEvent();
+EventManager.Send(testEvent);
+EventManager.Update();
